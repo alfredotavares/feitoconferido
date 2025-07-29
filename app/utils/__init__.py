667f442c2b8f76433c6e42085197bab1883e6b67
PedroSales117 <pedrohenrysalles@outlook.com>
2025-07-29 13:31:41 -0300

    feat: Remove módulos utilitários de autenticação e segurança não utilizados

--- app/utils/__init__.py
+++
@@ -1,8 +0,0 @@
-"""
-Utilitários do sistema Conferido
-"""
-
-from .security_validator import SecurityValidator, AuditLogger, RateLimiter, SessionManager
-
-__all__ = ['SecurityValidator', 'AuditLogger', 'RateLimiter', 'SessionManager']
-
